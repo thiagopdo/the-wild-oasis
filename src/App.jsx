d694aa03920a84c0d6bcc3dc9b024bf5cbeb858e
thiagopdo <thiago.falconer@gmail.com>
2024-02-12 15:19:22 -0300

    refactored input and button elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -30,13 +16,16 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("Check in")}>Check In</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check In</Button>
 
-      <Input type="number" placeholder="Number of guests" />
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
