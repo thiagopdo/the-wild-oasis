87afb44990e1d7017e80ffdf77f5b9928d415651
thiagopdo <thiago.falconer@gmail.com>
2024-02-13 19:26:36 -0300

    implemented cabin images

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,8 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+
+      <img src="https://nruobkekgbifyuwmkggz.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-02-13T22%3A23%3A23.363Z" />
     </Row>
   );
 }
