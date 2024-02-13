e951c542633984a422fb7b2e29ce0f86d06e315b
thiagopdo <thiago.falconer@gmail.com>
2024-02-13 16:35:04 -0300

    built sidebar and header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,13 @@
+import styled from "styled-components";
+
+const StylerHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
 function Header() {
-  return (
-    <header>
-      HEADER
-    </header>
-  )
+  return <StylerHeader>HEADER</StylerHeader>;
 }
 
-export default Header
+export default Header;
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,9 +1,15 @@
+import styled from "styled-components";
+
+const StylerSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
+`;
+
 function Sidebar() {
-  return (
-    <aside>
-      SIDEBAR
-    </aside>
-  )
+  return <StylerSidebar>SIDEBAR</StylerSidebar>;
 }
 
-export default Sidebar
+export default Sidebar;
