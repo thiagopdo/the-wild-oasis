4e9f4821f7fb9bc8854fa09f82e738a10ce8e7f3
thiagopdo <thiago.falconer@gmail.com>
2024-02-14 17:19:45 -0300

    implemented delete cabin functionality

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,7 +5,18 @@ export async function getCabins() {
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could no be loaded");
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
