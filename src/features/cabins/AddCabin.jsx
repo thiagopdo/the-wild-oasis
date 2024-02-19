d811f6bb0b741269fd0f118125805c7972987da4
thiagopdo <thiago.falconer@gmail.com>
2024-02-19 14:16:21 -0300

    added modal hook to the edit functionality

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,7 +1,6 @@
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import CabinTable from "./CabinTable";
 
 /**
  *
@@ -10,21 +9,16 @@ import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
