5170236adbc155222d9701522d61bd645ce654df
thiagopdo <thiago.falconer@gmail.com>
2024-02-22 17:51:17 -0300

    filtering more flexible to get the method to filter

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,8 +9,9 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   //FILTER
-  if (filter !== null) query = query.eq(filter.field, filter.value);
-  
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
   const { data, error } = await query;
 
   if (error) {
