e4e3e4c31745b44de60bc4f90044265dca23469d
thiagopdo <thiago.falconer@gmail.com>
2024-02-22 17:03:26 -0300

    created bookings table

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -2,9 +2,13 @@ import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
+import { useBookings } from "./useBookings";
+import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isLoading } = useBookings();
+
+  if (isLoading) return <Spinner />;
 
   if (!bookings.length) return <Empty resource="bookings" />;
 
