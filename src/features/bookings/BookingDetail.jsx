3184a42c451ee8cce9b7d4c6a070ce71541f89b4
thiagopdo <thiago.falconer@gmail.com>
2024-02-25 15:53:46 -0300

    added details to booking page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -43,7 +43,7 @@ function BookingDetail() {
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
-      {/* <BookingDataBox booking={booking} /> */}
+      <BookingDataBox booking={booking} />
 
       <ButtonGroup>
         <Button variation="secondary" onClick={moveBack}>
