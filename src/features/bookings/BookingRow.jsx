96f39f3ed7b5c3ad31882282e543cae0cb5fb888
thiagopdo <thiago.falconer@gmail.com>
2024-02-26 17:46:36 -0300

    implemented delete functionality on bookings row as a modal

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,23 +1,23 @@
 import styled from "styled-components";
-import { format, isToday } from "date-fns";
-
-import Tag from "../../ui/Tag";
-import Table from "../../ui/Table";
-
-import { formatCurrency } from "../../utils/helpers";
-import { formatDistanceFromNow } from "../../utils/helpers";
-import Menus from "../../ui/Menus";
 import {
-  HiArrowDown,
   HiArrowDownOnSquare,
   HiArrowUpOnSquare,
   HiEye,
   HiTrash,
 } from "react-icons/hi2";
-import { Navigate, useNavigate } from "react-router-dom";
-import { useCheckout } from "../check-in-out/useCheckout";
+import { useNavigate } from "react-router-dom";
+import { format, isToday } from "date-fns";
+
+import Tag from "../../ui/Tag";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+
+import { formatCurrency } from "../../utils/helpers";
+import { formatDistanceFromNow } from "../../utils/helpers";
+import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -62,6 +62,7 @@ function BookingRow({
 }) {
   const navigate = useNavigate();
   const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeleting } = useDeleteBooking();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -130,7 +131,10 @@ function BookingRow({
         </Menus.Menu>
 
         <Modal.Window name="delete">
-          <ConfirmDelete resourceName='booking' onConfirm={()=>{}} />
+          <ConfirmDelete
+            resourceName="booking"
+            onConfirm={() => deleteBooking(bookingId)}
+          />
         </Modal.Window>
       </Modal>
     </Table.Row>
--- src/features/bookings/useDeleteBooking.js
+++ src/features/bookings/useDeleteBooking.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
 
-export function useDeleteDelete() {
+export function useDeleteBooking() {
   const queryClient = useQueryClient();
 
   const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
