100b80d64df38be2936353bf6b546869175c39c5
thiagopdo <thiago.falconer@gmail.com>
2024-02-27 23:34:46 -0300

    styled header menu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,6 +6,11 @@ const StylerHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
