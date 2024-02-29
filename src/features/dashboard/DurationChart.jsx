854b17df937da09d74d72c6066d65028f18021d8
thiagopdo <thiago.falconer@gmail.com>
2024-02-29 17:12:56 -0300

    added the bookings data to the pie chart

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import Heading from "../../ui/Heading";
-import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -23,42 +31,42 @@ const ChartBox = styled.div`
 const startDataLight = [
   {
     duration: "1 night",
-    value: 5,
+    value: 0,
     color: "#ef4444",
   },
   {
     duration: "2 nights",
-    value: 3,
+    value: 0,
     color: "#f97316",
   },
   {
     duration: "3 nights",
-    value: 2,
+    value: 0,
     color: "#eab308",
   },
   {
     duration: "4-5 nights",
-    value: 4,
+    value: 0,
     color: "#84cc16",
   },
   {
     duration: "6-7 nights",
-    value: 1,
+    value: 0,
     color: "#22c55e",
   },
   {
     duration: "8-14 nights",
-    value: 4,
+    value: 0,
     color: "#14b8a6",
   },
   {
     duration: "15-21 nights",
-    value: 1,
+    value: 0,
     color: "#3b82f6",
   },
   {
     duration: "21+ nights",
-    value: 1,
+    value: 0,
     color: "#a855f7",
   },
 ];
@@ -66,42 +74,42 @@ const startDataLight = [
 const startDataDark = [
   {
     duration: "1 night",
-    value: 4,
+    value: 0,
     color: "#b91c1c",
   },
   {
     duration: "2 nights",
-    value: 3,
+    value: 0,
     color: "#c2410c",
   },
   {
     duration: "3 nights",
-    value: 5,
+    value: 0,
     color: "#a16207",
   },
   {
     duration: "4-5 nights",
-    value: 4,
+    value: 0,
     color: "#4d7c0f",
   },
   {
     duration: "6-7 nights",
-    value: 7,
+    value: 0,
     color: "#15803d",
   },
   {
     duration: "8-14 nights",
-    value: 1,
+    value: 0,
     color: "#0f766e",
   },
   {
     duration: "15-21 nights",
-    value: 2,
+    value: 0,
     color: "#1d4ed8",
   },
   {
     duration: "21+ nights",
-    value: 3,
+    value: 0,
     color: "#7e22ce",
   },
 ];
@@ -134,13 +142,17 @@ function prepareData(startData, stays) {
 }
 
 function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+  const data = prepareData(startData, confirmedStays);
+
   return (
     <ChartBox>
       <Heading as="h2">Stay duration summary</Heading>
-      <ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={240}>
         <PieChart>
           <Pie
-            data={startDataLight}
+            data={data}
             nameKey="duration"
             dataKey="value"
             innerRadius={85}
