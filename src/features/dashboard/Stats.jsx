1af8ff184e5aa20d8d55332b68150f7e8b63da1b
thiagopdo <thiago.falconer@gmail.com>
2024-02-29 15:48:55 -0300

    build stats for the dashboard

---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,56 @@
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  //1. number bookings
+  const numBookings = bookings.length;
+
+  //2. sales
+  //reduce() returns summed up values in an array, in one value
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  //3. total checkins
+  const checkins = confirmedStays.length;
+
+  //4. occupancy rate => num checked in nights / all nights
+  const occupation =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Checkin ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
